94baf64aaf9611e94c2e53f95f6af59ea25c514c
Roberto Natella <roberto.natella@unina.it>
2023-04-13 21:49:22 +0200

    buffer overflow

--- buffer-overflow/challenge/wisdom-alt.c
+++ buffer-overflow/challenge/wisdom-alt.c
@@ -14,7 +14,7 @@ char secret[] = "secret key";
 int infd = 0; /* stdin */
 int outfd = 1; /* stdout */
 
-#define DATA_SIZE 128
+#define DATA_SIZE 512
 
 typedef struct _WisdomList {
   struct  _WisdomList *next;
@@ -47,6 +47,7 @@ void get_wisdom(void) {
       l = l->next;
     }
   }
+  write(outfd, "\n", 1);
   return;
 }
 
@@ -78,6 +79,8 @@ void put_wisdom(void) {
       v->next = l;
     }
   }
+
+  write(outfd, "\n", 1);
 
   return;
 }
